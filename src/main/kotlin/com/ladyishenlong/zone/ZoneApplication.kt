ba9426e9222a0d3e4f18fcb22de5cde4753877ed
ruanchenhao <ruanchenhao@sanjiang.com>
2019-01-17 15:19:55 +0800

    添加注释

--- src/main/kotlin/com/ladyishenlong/zone/ZoneApplication.kt
+++ src/main/kotlin/com/ladyishenlong/zone/ZoneApplication.kt
@@ -4,6 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
+/**
+ * 这里是服务注册中心
+ */
 @EnableEurekaServer
 @SpringBootApplication
 class ZoneApplication
--- zone-gateway/src/main/kotlin/com/ladyishenlong/zonegateway/test/TestService.kt
+++ zone-gateway/src/main/kotlin/com/ladyishenlong/zonegateway/test/TestService.kt
@@ -4,10 +4,17 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.stereotype.Repository
 import org.springframework.web.bind.annotation.GetMapping
 
+/**
+ * FeignClient 的 name 是被调用端的 spring.application.name
+ *
+ */
 @Repository
 @FeignClient(name = "zone-test")
 interface TestService {
 
+    /**
+     * 这里的地址是远端 controller 的地址一致
+     */
     @GetMapping("/zonetest-test/test")
     fun test(): TestModel
 
